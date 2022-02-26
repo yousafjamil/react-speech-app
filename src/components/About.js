d1bd24d428e11bad8b9f32bfed5ce303ac400cd8
yousaf jamil <yousafjamil200@gmail.com>
2022-02-26 13:51:59 +0500

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,31 +1,10 @@
-import React from 'react';
+import React,{useState} from 'react'
 
-const About = () => {
+function About() {
+ 
+  return {
+ 
+  }
+}
 
-
-  return  <div className='container  py-2 mt-2'>
-
-  <div className='row'>
-    <div className='col-sm-8'>
-   
-      <input   placeholder='enter item you wanna to add ina list'  className='form-control'/>
-<select>
-  <option>H</option>
-</select>
-
-
-
-    </div>
-   {/*  */}
-  
-   <div className='col-sm-4'>
-  
-  
-   </div>
-  </div>
-  
-  
-    </div>;
-};
-
-export default About;
+export default About
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,37 @@
-import React, { useState } from 'react';
-import About from './About';
-import { useSpeechSynthesis } from 'react-speech-kit';
-
+import React, { useState } from 'react'
+import data from '../data'
 const Home = () => {
 
-  const [value, setValue] = useState('');
-  const { speak } = useSpeechSynthesis();
-  
-
-  return <div className='container  py-5 mt-5 jumbotron'>
-
-<textarea rows={10} cols={40}
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-      /><br />
-      <button onClick={() => speak({ text: value })} className='btn btn-info w-50'>Speak</button>
+  const [list, setList] = useState(data)
+  console.log(list)
 
 
-  </div>;
+  return (
+    <div className='container  py-5 mt-5 '  >
+      <div className='row'>
+        <div className='col-sm-8 col-md-8 col-lg-8 offset-sm-2 offset-md-2 offset-lg-2'>
+          {list.map((item, index) => {
+            return (
+              <>
+                <div class="card mb-5 jumbotron" >
+                  <img class="card-img-top" src={item.image} alt="Card image cap" style={{ maxWidth: '400px', justifyContent: 'center', margin: '1rem', height: '400px' }} />
+                  <div class="card-body">
+                    <h5 class="card-title">{item.name}</h5>
+                    <p class="card-text">{item.age}</p>
+
+                  </div>
+                </div>
+
+              </>
+            )
+          })}
+          <button className='btn btn-outline-info w-75' onClick={() => setList('')}>Clear All </button>
+        </div>
+
+      </div>
+
+
+    </div>);
 
 };
 
